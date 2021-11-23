a5f077fa8d32e0ed77929a319caa1c493a230d9a
Keisay <kinsay.soundcloud@gmail.com>
2021-11-23 22:38:29 +0100

    modifying python script

--- amenohabakiri.py
+++ amenohabakiri.py
@@ -46,3 +46,94 @@ fileData = fileData.replace(hostName, rightHostname)
 with open("WorkingTimes.vue", 'w') as file:
         file.write(fileData)
 file.close()
+
+file = open("Home.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("Home.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("TeamCharts.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("TeamCharts.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("SelectedEmployeeCharts.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("SelectedEmployeeCharts.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("ManagerCharts.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("ManagerCharts.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("CurrentUserCharts.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("CurrentUserCharts.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("SignUp.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("SignUp.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("Logout.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("Logout.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("Login.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("Login.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("UserTeamManager.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("UserTeamManager.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("UserManagementComponent.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("UserManagementComponent.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("TeamManager.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("TeamManager.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("SideBarComponent.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("SideBarComponent.vue", 'w') as file:
+        file.write(fileData)
+file.close()
+
+file = open("AdminComponent.vue", "r")
+fileData = file.read()
+fileData = fileData.replace(hostName, rightHostname)
+with open("AdminComponent.vue", 'w') as file:
+        file.write(fileData)
+file.close()
